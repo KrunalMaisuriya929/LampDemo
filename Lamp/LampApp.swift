8384f8f85c8a47860cc222a8bbcbd8453b167520
KrunalMaisuriya929 <krunalmaisuriya929@gmail.com>
2024-07-29 12:28:04 +0100

    Initial Commit

---
+++ Lamp/LampApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampApp.swift
+//  Lamp
+//
+//  Created by Krunal on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
